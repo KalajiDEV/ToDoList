fa307416ff7653e45b5322ad02f3c9c2f2f2085a
KalajiDEV <114266607+KalajiDEV@users.noreply.github.com>
2022-10-26 02:05:44 +0200

    More responsive

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const addBtn = document.querySelector(".addBtn");
-const tasksbox = document.getElementById("tasksbox");
 const inputTitle = document.getElementsByTagName("input")[0];
 const inputtask = document.getElementsByTagName("input")[1];
 const form = document.querySelector("form");
+const tasksbox = document.getElementById("tasksbox");
+const tools = document.getElementById("tools");
 
+// colro Change
+const changeColor = document
+  .getElementById("changeColor")
+  .addEventListener("click", (params) => {
+    document.body.classList.toggle("changeColor");
+  });
+
+// Create my Task
 form.addEventListener("submit", (eo) => {
   eo.preventDefault();
-
-  const datum = new Date();
   const task = `<section>
   <h3>${inputTitle.value}</h3>
   <p>${inputtask.value}</p>
@@ -15,16 +21,19 @@ form.addEventListener("submit", (eo) => {
   <i class="fa-sharp fa-solid fa-trash"></i>
   <i class="fa-solid fa-star"></i>
   <i class="fa-solid fa-share"></i>
-</div>
+  </div>
   </section>`;
   tasksbox.innerHTML += task;
-  eo.preventDefault();
   inputTitle.value = "";
   inputtask.value = "";
 });
 
-const changeColor = document
-  .getElementById("changeColor")
-  .addEventListener("click", (params) => {
-    document.body.classList.toggle("changeColor");
-  });
+// Delete Task // favorite
+
+tasksbox.addEventListener("click", (eo) => {
+  if (eo.target.className == "fa-sharp fa-solid fa-trash") {
+    eo.target.parentElement.parentElement.remove();
+  } else if (eo.target.className == "fa-solid fa-star") {
+    eo.target.parentElement.parentElement.style.background = "yellow";
+  }
+});
